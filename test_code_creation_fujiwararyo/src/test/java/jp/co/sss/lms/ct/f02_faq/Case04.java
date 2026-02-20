e52356ebfc2bcf8fb26692006c25cf4eeb93dd61
edu <edu@B65DS>
2026-02-20 17:14:47 +0900

    add:Case04.Java ヘルプ画面、よくある画面質問に遷移するテストコード追加 操作は以下の通り 機能ボタン:機能 ヘルプリンク:ヘルプ
    WebElementのlinkTextを使用して、機能とヘルプの値を取得し、ヘルプ画面に遷移することができるか確認するテストを作成した
    期待値
    タイトル:ヘルプ | LMS
    ヘルプ画面に遷移する
    
    よくある質問の操作は以下の通り
    よくある質問リンク:よくある質問
    新しくタグが作成される為、ウィンドウ名でタグを呼ぶ
    指定した要素が画面上に表示されるまで5秒間待つ
    
    WebElementのclassNameを使用して、よくある質問のクラス属性を取得する
    WebElementのlinkTextを使用して、よくある質問の値を取得し、よくある質問画面に遷移すること
    期待値
    タイトル:よくある質問 | LMS
    よくある質問タグを作成し、画面遷移する

--- test_code_creation_fujiwararyo/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_fujiwararyo/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,28 +42,74 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		//		指定のURLの画面を開く
+		goTo("http://localhost:8080/lms");
+
+		//		titleの取得とアサーション
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+
+		WebElement passElement = webDriver.findElement(By.id("password"));
+		passElement.clear();
+		passElement.sendKeys("StudentAA01A");
+
+		WebElement clickElement = webDriver.findElement(By.className("btn"));
+		clickElement.click();
+
+		pageLoadTimeout(30);
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		WebElement linkElement = webDriver.findElement(By.linkText("機能"));
+		linkElement.click();
+
+		WebElement titleElement = webDriver.findElement(By.linkText("ヘルプ"));
+		titleElement.click();
+
+		pageLoadTimeout(30);
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
-	}
 
+		WebElement classElement = webDriver.findElement(By.className("panel-body"));
+
+		WebElement linkElement = webDriver.findElement(By.linkText("よくある質問"));
+		linkElement.click();
+
+		//		タイトル名:よくある質問タグを呼ぶ
+		webDriver.switchTo().window("");
+
+		//		指定した要素が画面上に表示されるまで待つ 
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.titleIs("よくある質問 | LMS"));
+		getEvidence(new Object() {
+		});
+	}
 }
