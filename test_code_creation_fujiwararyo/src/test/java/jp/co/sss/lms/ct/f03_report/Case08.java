9e63a471e08cdaeef0a58cf6348a57f1717e4b68
edu <edu@B65DS.mshome.net>
2026-02-25 14:13:02 +0900

    add:Case08.Java レポート機能(週報)テストコード作成 findElementを使用して研修日2022年10月2日の詳細ボタンを押下しタイトル:セクション詳細|LMSが見つかるまで5秒間待機し200ピクセル下にスクロールする WebElementのxpathを使用して要素を取得し「提出済み週報【デモ】を確認する」と同じか比較する
    期待値は以下の通り
    詳細ボタン詳細
    
    findElementのxpathを使用して「提出済み週報【デモ】を確認する」ボタンを押下しタイトル:レポート登録 |
    LMSが見つかるまで5秒間待機し、500ピクセル下にスクロールする
    getTitleを使用してタイトルがレポート登録 | LMSと同じか比較する
    
    期待値は以下の通り
    提出済み週報【デモ】を確認するボタン：提出済み週報【デモ】を確認する
    
    WebElementのxpathを使用して所感のテキストタグをクリアにし、「修正しました。」と入力する
    findElementのxpathを使用して「提出する」ボタンを押下し、タイトルがセクション詳細 | LMSが見つかるまで5秒間待機する
    getTitleを使用してタイトルが「セクション詳細 | LMS」と同じか比較する
    期待値は以下の通り
    所感テキストタグ: 修正しました
    提出するボタン: 提出する
    
    findElementを使用し「ようこそ受講生AA1さん」ボタンを押下しタイトル:ユーザー詳細がみつかるまで5秒間待機し下に500ピクセルスクロールする
    getTitleを使用してタイトル：「ユーザー詳細」と同じか比較する
    期待値は以下の通り
    ようこそ受講生AA1さんボタン:ようこそ受講生AA1さん
    
    fubdEkenebtのxpathを使用して週報「デモ」の「詳細」ボタンを押下し、タイトル：レポート詳細 | LMSが見つかるまで5秒間待機する
    WebElementのxpathを使用して所感のテキストタグを取得する
    getTextを使用して「修正しました。」と同じか比較する
    期待値は以下の通り
    詳細ボタン：詳細

--- test_code_creation_fujiwararyo/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
+++ test_code_creation_fujiwararyo/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト レポート機能
@@ -35,49 +42,122 @@ public class Case08 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// 指定のURLの画面を開く
+		goTo("http://localhost:8080/lms");
+
+		// titleの取得とアサーション
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+
+		WebElement passElement = webDriver.findElement(By.id("password"));
+		passElement.clear();
+		passElement.sendKeys("StudentAA01A");
+
+		WebElement clickElement = webDriver.findElement(By.className("btn"));
+		clickElement.click();
+
+		pageLoadTimeout(30);
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 提出済の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		webDriver
+				.findElement(By.xpath(
+						"//tr[td[contains(normalize-space(), '2022年10月2日(日)')]][td[3][contains(., '提出済み')]]/td[5]"))
+				.click();
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.titleIs("セクション詳細 | LMS"));
+		scrollTo("200");
+		
+		WebElement button = webDriver.findElement(By.xpath("//input[@value='提出済み週報【デモ】を確認する']"));
+		assertEquals("提出済み週報【デモ】を確認する", button.getAttribute("value"));
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「確認する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		webDriver.findElement(By.xpath("//input[@value='提出済み週報【デモ】を確認する']")).click();
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.titleIs("レポート登録 | LMS"));
+		
+		scrollTo("500");
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しセクション詳細画面に遷移")
 	void test05() {
-		// TODO ここに追加
+		
+		//		所感のテキストタグをクリアにし、記入する
+		WebElement xpathElement = webDriver.findElement(By.xpath("//*[@id='content_1']"));
+		xpathElement.clear();
+		xpathElement.sendKeys("修正しました。");
+
+		webDriver.findElement(By.xpath("//button[@type='submit']")).click();
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.titleIs("セクション詳細 | LMS"));
+		
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test06() {
-		// TODO ここに追加
+
+		webDriver.findElement(By.partialLinkText("ようこそ受講生ＡＡ１さん")).click();
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.titleIs("ユーザー詳細"));
+		
+		scrollTo("500");
+		assertEquals("ユーザー詳細", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 該当レポートの「詳細」ボタンを押下しレポート詳細画面で修正内容が反映される")
 	void test07() {
-		// TODO ここに追加
-	}
 
+		webDriver.findElement(By.xpath("//tr[td[text()='週報【デモ】']]/td[5]/form[1]")).click();
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.titleIs("レポート詳細 | LMS"));
+
+		//		修正しました。という文字列を取得する
+		WebElement textElement = webDriver.findElement(By.xpath("//tr[th[text()='所感']]/td"));
+		assertEquals("修正しました。", textElement.getText());
+		getEvidence(new Object() {
+		});
+	}
 }
