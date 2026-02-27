907e28ec3d3137e5638e9d22a990dda024db7528
edu <edu@B65DS.mshome.net>
2026-02-27 17:20:42 +0900

    add:Case11.Java 受講生 勤怠直接編集 正常系テストコード作成
    test04
    webDrver.findElementのlinkTextを使用して「勤怠情報を直接編集する」リンクを押下する
    ページロードタイムアウトが完了するまで３０秒間待機する
    
    test05
    WebElementを使用してidを取得する
    selectByIndexメソッドを使用してドロップダウンリストに値を取得する
    下に５００ピクセルスクロールする
    更新ボタンを押下する
    waitを使用して、アラートが表示されるまで１０秒間待機する
    acceptを使用して「OK」ボタンを押下する
    ページロードタイムアウトが完了するまで３０秒間待機する

--- test_code_creation_fujiwararyo/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
+++ test_code_creation_fujiwararyo/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +44,105 @@ public class Case11 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// 指定のURLの画面を開く
+		goTo("http://localhost:8080/lms");
+
+		// titleの取得とアサーション
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("StudentAA02");
+
+		WebElement passElement = webDriver.findElement(By.id("password"));
+		passElement.clear();
+		passElement.sendKeys("StudentAA02A");
+
+		WebElement clickElement = webDriver.findElement(By.className("btn"));
+		clickElement.click();
+
+		pageLoadTimeout(30);
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		webDriver.findElement(By.linkText("勤怠")).click();
+		pageLoadTimeout(30);
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		webDriver.findElement(By.linkText("勤怠情報を直接編集する")).click();
+		pageLoadTimeout(30);
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 すべての研修日程の勤怠情報を正しく更新し勤怠管理画面に遷移")
 	void test05() {
-		// TODO ここに追加
+
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
+		WebElement startHour = webDriver.findElement(By.id("startHour0"));
+
+		Select selectReason = new Select(startHour);
+		selectReason.selectByIndex(6);
+
+		WebElement stopHour = webDriver.findElement(By.id("startMinute0"));
+		Select selectReason2 = new Select(stopHour);
+		selectReason2.selectByIndex(1);
+
+		WebElement endHour = webDriver.findElement(By.id("endHour0"));
+
+		Select endReason = new Select(endHour);
+		endReason.selectByIndex(8);
+
+		WebElement endStopHour = webDriver.findElement(By.id("endMinute0"));
+
+		Select endStopHour2 = new Select(endStopHour);
+		endStopHour2.selectByIndex(1);
+		scrollTo("500");
+
+		webDriver.findElement(By.xpath("//input[@value='更新']")).click();
+		wait.until(ExpectedConditions.alertIsPresent());
+
+		// アラートに制御を切り替える
+		Alert alert = webDriver.switchTo().alert();
+
+		// OKボタン（確定）をクリック
+		alert.accept();
+		pageLoadTimeout(30);
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 }
