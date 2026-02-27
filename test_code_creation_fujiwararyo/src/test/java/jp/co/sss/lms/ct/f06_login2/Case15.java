619a5b334c5c0af1db8e14200039f15b61ca23a2
edu <edu@B65DS.mshome.net>
2026-02-27 14:04:40 +0900

    add:Case15.Java 受講生 初回ログイン 利用規約に不同意テストコード作成
    test03
    webDriverのfindElementを使用してclassName「btn-primary」の「同意します」ボタンを押下する
    findElementを使用して、className「error」の要素を取得する
    assertEqualsを使用して、error変数とエラーメッセージが同じかを比較する

--- test_code_creation_fujiwararyo/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_fujiwararyo/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,21 +41,51 @@ public class Case15 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		
+		// 指定のURLの画面を開く
+		goTo("http://localhost:8080/lms");
+
+		// titleの取得とアサーション
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.clear();
+		loginId.sendKeys("StudentAA02");
+
+		WebElement passElement = webDriver.findElement(By.id("password"));
+		passElement.clear();
+		passElement.sendKeys("StudentAA02");
+
+		WebElement clickElement = webDriver.findElement(By.className("btn"));
+		clickElement.click();
+		pageLoadTimeout(10);
+		
+		
+		assertEquals("セキュリティ規約 | LMS", webDriver.getTitle());
+		scrollTo("300");
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
 	void test03() {
-		// TODO ここに追加
+		
+		webDriver.findElement(By.className("btn-primary")).click();
+		WebElement error = webDriver.findElement(By.className("error"));
+		assertEquals("セキュリティ規約への同意は必須です。", error.getText());
+		scrollTo("300");
 	}
 
 }
